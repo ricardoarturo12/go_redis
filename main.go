45f9ca422ca6496d4df011b770a3bba2f7a4949b
ricardo121990 <ricardo121990@gmail.com>
2022-04-26 16:55:31 -0400

    implementado el method get

--- main.go
+++ main.go
@@ -3,44 +3,77 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/gomodule/redigo/redis"
 	"github.com/joho/godotenv"
 )
 
-type Album struct {
-	Title  string  `redis:"title"`
-	Artist string  `redis:"artist"`
-	Price  float64 `redis:"price"`
-	Likes  int     `redis:"likes"`
-}
-
 
 func main() {
+	// Initialize a connection pool and assign it to the pool global
+	// variable.
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
 
-	conn, err := redis.Dial("tcp", "localhost:8080",
-		// redis.DialUsername("username"),
-		redis.DialPassword("12345"))
-	if err != nil {
-		log.Fatal(err)
+	pool = &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:8080",
+				   			 redis.DialPassword(os.Getenv("PASSWORD")))
+		},
 	}
 
-	defer conn.Close()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/album", showAlbum)
+	log.Println("Listening on :4000...")
+	http.ListenAndServe(":4000", mux)
+}
 
-	values, err := redis.Values(conn.Do("HGETALL", "album:1"))
-	if err != nil {
-		log.Fatal(err)
+func showAlbum(w http.ResponseWriter, r *http.Request) {
+	// Unless the request is using the GET method, return a 405 'Method
+	// Not Allowed' response.
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, http.StatusText(405), 405)
+		return
 	}
 
-	var album Album
-
-	err = redis.ScanStruct(values, &album)
-	if err != nil{
-		log.Fatal(err)
+	// Retrieve the id from the request URL query string. If there is
+	// no id key in the query string then Get() will return an empty
+	// string. We check for this, returning a 400 Bad Request response
+	// if it's missing.
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, http.StatusText(400), 400)
+		return
 	}
-	fmt.Printf("%+v", album)
+	// Validate that the id is a valid integer by trying to convert it,
+	// returning a 400 Bad Request response if the conversion fails.
+	if _, err := strconv.Atoi(id); err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	// Call the FindAlbum() function passing in the user-provided id.
+	// If there's no matching album found, return a 404 Not Found
+	// response. In the event of any other errors, return a 500
+	// Internal Server Error response.
+	bk, err := FindAlbum(id)
+	if err == ErrNoAlbum {
+		http.NotFound(w, r)
+		return
+	} else if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	// Write the album details as plain text to the client.
+	fmt.Fprintf(w, "%s by %s: £%.2f [%d likes] \n", bk.Title, bk.Artist, bk.Price, bk.Likes)
 }
